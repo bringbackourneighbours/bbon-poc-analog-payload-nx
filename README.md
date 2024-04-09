d5188009189122aa7a357ae84c53682c7de0f91c
Johannes Voigt (Finatix) <j.voigt@finatix.de>
2024-04-09 08:40:39 +0200

    payload within express not working

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # BbonPocAnalogPayloadNx
 
+- analog is working
+- payload is not working
+  - nx run payload-api:serve =>
+  - /Workspace/bbon/bbon-poc-analog-payload-nx/node_modules/payload/dist/admin/components/elements/Banner/index.scss:1
+    @import '../../../scss/styles.scss';
+    ^
+    SyntaxError: Invalid or unexpected token
+
+
 ## Start it
 
 You need node, docker, dockes compose.
--- apps/payload-api/src/main.ts
+++ apps/payload-api/src/main.ts
@@ -1,21 +1,34 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
+import 'dotenv/config';
 import express from 'express';
-import * as path from 'path';
+import payload from 'payload'
+import payloadConfig from './payload.config'
 
 const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
-
 app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to payload-api! This is jus an express server' });
+  res.send({message: 'Welcome to payload-api! This is jus an express server'});
 });
 
 const port = process.env.PORT || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
-});
-server.on('error', console.error);
+
+const start = async () => {
+  await payload.init({
+    config: payloadConfig,
+    secret: process.env.PAYLOAD_SECRET,
+    express: app,
+    onInit: async () => {
+      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
+    },
+
+  });
+
+  const server = app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}/api`);
+  });
+
+  server.on('error', console.error);
+}
+
+start().then(() => {
+  console.log(`Started!`);
+})
